400157c0908adc60ea95e47ac75f455345332eb0
Richard Liebscher <r1tschy@posteo.de>
2022-05-14 11:54:29 +0200

    Improve CI

--- noxfile.py
+++ noxfile.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 from pathlib import Path
 
 import nox
@@ -11,16 +12,16 @@ default_python_version = "3.9"
 python_versions = ["3.10", "3.9", "3.8", "3.7"]
 
 nox.needs_version = ">= 2021.6.6"
-nox.options.sessions = ("test", "doctest", "lint", "typecheck")
+nox.options.sessions = ("tests", "xdoctest", "lint", "mypy", "docs-build")
 
 
 @session(python=python_versions)
-def test(session: Session) -> None:
+def tests(session: Session) -> None:
     """Run the test suite."""
     session.install(".")
-    session.install("coverage[toml]", "pytest", "pygments")
+    session.install("coverage[toml]", "pytest", "pygments", "typeguard")
     try:
-        session.run("coverage", "run", "--parallel", "-m", "pytest", *session.posargs)
+        session.run("coverage", "run", "--parallel", "-m", "pytest", f"--typeguard-packages={package}", *session.posargs)
     finally:
         if session.interactive:
             session.notify("coverage", posargs=[])
@@ -40,7 +41,7 @@ def coverage(session: Session) -> None:
 
 
 @nox.session(python=python_versions)
-def doctest(session: Session) -> None:
+def xdoctest(session: Session) -> None:
     """Run examples with xdoctest."""
     if session.posargs:
         args = [package, *session.posargs]
@@ -77,14 +78,38 @@ def lint(session):
 
 
 @nox.session(python=[default_python_version])
-def typecheck(session):
+def mypy(session):
     args = session.posargs or ["src"]
     session.install("mypy")
     session.run("mypy", *args)
 
 
-@nox.session(python=[default_python_version])
-def doc(session: Session) -> None:
+@session(name="docs-build", python=python_versions[0])
+def docs_build(session: Session) -> None:
     """Build the documentation."""
-    session.install("sphinx", "sphinx-autodoc-typehints")
-    session.run("sphinx-build", "docs", "build/docs")
+    args = session.posargs or ["docs", "docs/_build"]
+    if not session.posargs and "FORCE_COLOR" in os.environ:
+        args.insert(0, "--color")
+
+    session.install(".")
+    session.install("sphinx", "sphinx-click", "furo", "myst-parser")
+
+    build_dir = Path("docs", "_build")
+    if build_dir.exists():
+        shutil.rmtree(build_dir)
+
+    session.run("sphinx-build", *args)
+
+
+@session(python=python_versions[0])
+def docs(session: Session) -> None:
+    """Build and serve the documentation with live reloading on file changes."""
+    args = session.posargs or ["--open-browser", "docs", "docs/_build"]
+    session.install(".")
+    session.install("sphinx", "sphinx-autobuild", "sphinx-click", "furo", "myst-parser")
+
+    build_dir = Path("docs", "_build")
+    if build_dir.exists():
+        shutil.rmtree(build_dir)
+
+    session.run("sphinx-autobuild", *args)
